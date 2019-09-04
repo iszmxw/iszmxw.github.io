ce7f0847c5a6a76752ed7412ff98bfb9fd7904b0
iszmxw <mail@54zm.com>
2019-09-04 17:47:21 +0800

    master

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,9 +1,21 @@
 module.exports = {
     title: '开发文档',  // 设置网站标题
-    dest: './dist',    // 设置输出目录
+    dest: './docs/docs',    // 设置输出目录
     base: '/', // 设置站点根路径
     repo: 'https://iszmxw.github.io/', // 添加 github 链接
     head: [
         ['link', { rel: 'icon', href: '/logo.png' }]
-    ]
+    ],
+    themeConfig: {
+        nav: [
+            { text: '首页', link: '/' },
+            { text: '商户后台', link: 'http://localhost:9528' },
+            { text: 'More', link: 'https://hai-fen.com/' },
+        ],
+        sidebar: [
+            ['/', '首页'],
+            ['/singe', '签名'],
+            ['/advert', '广告对接']
+        ]
+    }
 }
