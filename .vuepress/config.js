1de582ac8d01471fdf8e6890cc43e29eb48cf9ec
iszmxw <mail@54zm.com>
2019-09-04 13:10:07 +0800

    master

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,5 +2,8 @@ module.exports = {
     title: '开发文档',  // 设置网站标题
     dest: './dist',    // 设置输出目录
     base: '/', // 设置站点根路径
-    repo: 'https://iszmxw.github.io/' // 添加 github 链接
-  }
+    repo: 'https://iszmxw.github.io/', // 添加 github 链接
+    head: [
+        ['link', { rel: 'icon', href: '/logo.png' }]
+    ]
+}
